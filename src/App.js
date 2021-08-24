81a69683ded6f9f9556ac96772619fedc6d5b5e7
Lars Assen (student) <s1102416@student.windesheim.nl>
2021-08-24 21:33:24 +0200

    added some content to the app component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 
 function App() {
   return (
-    <div>
-      <h1 className="text-6xl">Hello world</h1>
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <img src="https://source.unsplash.com/random" className="w-full" alt="" />
+      <div className="px-6 py-4" >
+        <div className="font-bold text-purple-500 text-xl mb-2">
+          Photo by John Doe
+        </div>
+        <ul>
+          <li>
+            <strong>Views:</strong> 4000
+          </li>
+          <li>
+            <strong>Downloads:</strong> 30
+          </li>
+          <li>
+            <strong>Likes:</strong> 500
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
