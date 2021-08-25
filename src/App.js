c2848ac95cd15e6f8e526879dcb7c1888f7424d3
Lars Assen (student) <s1102416@student.windesheim.nl>
2021-08-25 21:45:53 +0200

    added usestate and useEffect to the app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState('');
+
+  useEffect(() => {
+    fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&
+    q=${term}&image_type=photo&pretty=true`)
+  })
+
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
       <img src="https://source.unsplash.com/random" className="w-full" alt="" />
