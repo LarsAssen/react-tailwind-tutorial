0eaaa3c43f9b49378412bdb22753eacb20c0f981
Lars Assen <juniorassen@hotmail.com>
2021-09-11 13:24:49 +0200

    finished the project

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,38 +1,34 @@
 import React from 'react'
 
 const ImageCard = ({image}) => {
+    const tags = image.tags.split(',');
+    
     return (
 <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img src="https://source.unsplash.com/random" className="w-full" alt="" />
+      <img src={image.webformatURL} className="w-full" alt="" />
       <div className="px-6 py-4" >
         <div className="font-bold text-purple-500 text-xl mb-2">
-          Photo by John Doe
+          Photo by {image.user}
         </div>
         <ul>
           <li>
-            <strong>Views:</strong> 4000
+            <strong>Views:</strong> {image.views}
           </li>
           <li>
-            <strong>Downloads:</strong> 30
+            <strong>Downloads:</strong> {image.downloads}
           </li>
           <li>
-            <strong>Likes:</strong> 500
+            <strong>Likes:</strong> {image.likes}
           </li>
         </ul>
       </div>
       <div className="px-6 py-4">
-        <span className="inline-block bg-gray-200 rounded-full
-        px-3 py-1 text-sm font-semibold text-gray-700 mr-2" >
-          #tag1
-        </span>
-        <span className="inline-block bg-gray-200 rounded-full
-        px-3 py-1 text-sm font-semibold text-gray-700 mr-2" >
-          #tag2
-        </span>
-        <span className="inline-block bg-gray-200 rounded-full
-        px-3 py-1 text-sm font-semibold text-gray-700 mr-2" >
-          #tag3
-        </span>
+        {tags.map((tag, index) => (
+                  <span key={index} className="inline-block bg-gray-200 rounded-full
+                  px-3 py-1 text-sm font-semibold text-gray-700 mr-2" >
+                    #{tag}
+                  </span>
+        ))}
       </div>
     </div>
     )
